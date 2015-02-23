f55bac958e8e4a18bda62607fcb95319b052b6ee
Jessica <jessica.battle@gmail.com>
2015-02-23 08:40:19 -0800

    added link to bootstrap cdn for styles; also redoing js file

--- youtube_bookmark.js
+++ youtube_bookmark.js
@@ -2,137 +2,46 @@
 //A Javascript bookmarking app for youtube
 
 
+///---------- Planning Section----------------------
+
 //Youtube API key:  AIzaSyB_W0TwsWSpJDba1Tm9eXOk20VSXGIACWw
+
+
+// 1.
+// user can enter in a search query and a bookmark name
+// -app will return both a list of youtube videos that match that search 
+// ---list will be capped at a certain number...say top 10
+// -app will add that bookmark to the running list of bookmarks
+
+// 2.
+// user can delete any bookmark from the list
+
+// 3.
+// user can delete all of the bookmarks
+
+// 4.
+// user can clear the list of videos
+
+// 5.
+// user can edit the name of a bookmark in the running list of bookmarks
+
+
+///----------End Planning Section----------------------
+
+
 
 //global variables
 var tags;		//array to hold tags the user creates
 
-function displayBookmarks()
-{
-	if(!sessionStorage.getItem("herePreviously")) 
-	{
-		sessionStorage.setItem("herePreviously", "true");
-		document.getElementById("welcomeMessage").innerHTML =
-			"Welcome to the Youtube Bookmark App";
-	} //end if
 
-	tags = [];
-	for(var i = 0; i < localStorage.length; ++i)
-	{
-		tags[i] =  localStorage.key(i);
-	}//end for
-
-	var table = "<table>" + 
-								"<tr class='head'>" +
-								"<th>Bookmark</th>" +
-								"<th>Delete</th>" +
-								"</tr>";
-
-	for(bookmark in tags)
-	{
-		table += "<tr>" +
-							"<th>" + tags[bookmark] + "</th>" +
-							"<td><input type='button' value='Delete'" +
-							"id='" + tags[bookmark] + "'" +
-							"onclick='deleteBookmark(id)'></td>" +
-						"</tr>";
-	} //end for
-
-	table += "</table>";
-
-	document.getElementById("searches").innerHTML = table;
-}//end function displayBookmarks
-
-function saveSearch()
-{
-	var query = document.getElementById("queryInput");
-	var tag = document.getElementById("tagQuery")
-	localStorage.setItem(tagQuery.value, queryInput.value);
-	search(query);
-
-	//clear out variables
-	query.value = "";
-	tag.value = "";
-
-	//reload the list of bookmarks
-	displayBookmarks();
-
-}//end function saveSearch
-
-function clearSearches()
-{
-	localStorage.clear();
-
-	//show cleared UI
-	displayBookmarks();
-}//end function clearSearches
-
-
-
-//delete one bookmark from the list
-function deleteBookmark(bookmark)
-{
-	//remove the associated record in local storage
-	localStorage.removeItem(bookmark);
-
-	//display the updated list of bookmarks
-	displayBookmarks();
-}//end function deleteBookmark
-
-
-//register event handlers for the save and clear buttons; also display
-//any saved bookmarks
+//register event listeners
 function start()
 {
-	if(typeof(localStorage) !== "undefined") 
-	{
-    document.getElementById("saveButton").addEventListener("click",
-		saveSearch,false);
-		document.getElementById("clearButton").addEventListener("click",
-		clearSearches,false);
+	document.getElementById("saveButton").addEventListener(
+		"click",saveBookmark,false);
 
-		displayBookmarks();
-	}//end if 
-	else 
-	{
-    alert("Sorry, this browser does not support local storage, so this app will not work");
-	}	
+	document.getElementById("clearButton").addEventListener(
+		"click", clearBookmarks,false);
 }//end function start
-
-
-//------------------Youtube API-----------------------
-
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('bookmark-search-results').innerHTML += responseString;
-}
-
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-
-function onYouTubeApiLoad() {
-    gapi.client.setApiKey('AIzaSyB_W0TwsWSpJDba1Tm9eXOk20VSXGIACWw');
-
-    // search();
-}
-
-function search(query) {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        'q': query
-    });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
-
-function onSearchResponse(response) {
-    showResponse(response);
-}
 
 window.addEventListener("load", start, false);
