8e4c6b06b3e74b94708284cc2ad38af30b15b85e
Jessica <jessica.battle@gmail.com>
2015-02-25 08:51:30 -0800

    set up save bookmark and delete all functionality

--- youtube_bookmark.js
+++ youtube_bookmark.js
@@ -10,20 +10,23 @@
 // 1.
 // user can enter in a search query and a bookmark name
 // -app will return both a list of youtube videos that match that search 
-// ---list will be capped at a certain number...say top 10
-// -app will add that bookmark to the running list of bookmarks
+// (XXXRemove from ScopeXXX)---list will be capped at a certain number...say top 10
+// (DONE!!) -app will add that bookmark to the running list of bookmarks
 
 // 2.
 // user can delete any bookmark from the list
 
 // 3.
-// user can delete all of the bookmarks
+// (DONE!!) user can delete all of the bookmarks
 
 // 4.
 // user can clear the list of videos
 
 // 5.
-// user can edit the name of a bookmark in the running list of bookmarks
+// (XXXRemove from ScopeXXX)user can edit the name of a bookmark in the running list of bookmarks
+
+// 6.
+// fix up the styles
 
 
 ///----------End Planning Section----------------------
@@ -31,17 +34,69 @@
 
 
 //global variables
-var tags;		//array to hold tags the user creates
+var all_bookmarks;		//array to hold bookmarks the user creates
 
+//save bookmark to local storage and display bookmarks in list on html page
+function saveBookmark()
+{
+
+	var query  = document.getElementById("queryInput");
+	var bookmark  = document.getElementById("bookmarkQuery");
+
+	localStorage.setItem(bookmark.value, query.value);
+
+	displayBookmarks();
+}//end function saveBookmark
+
+function displayBookmarks()
+{
+	all_bookmarks = [];	//initialize all_bookmarks variable to an array
+	length = localStorage.length;
+
+	for(var i = 0; i < length; ++i)
+	{
+		all_bookmarks[i] =  localStorage.key(i);
+	}//end for
+
+	var table = "<table>" + 
+								"<tr class='head'>" +
+								"<th>Bookmark</th>" +
+								"<th>Delete</th>" +
+								"</tr>";
+
+	for(bookmark in all_bookmarks)
+	{
+		table += "<tr>" +
+							"<th>" + all_bookmarks[bookmark] + "</th>" +
+							"<td><input type='button' value='Delete'" +
+							"id='" + all_bookmarks[bookmark] + "'" +
+							"onclick='deleteBookmark(id)'></td>" +
+						"</tr>";
+	} //end for
+
+	table += "</table>";
+
+	document.getElementById("bookmarks").innerHTML = table;
+}//end function displayBookmarks
+
+function clearBookmarks()
+{
+	localStorage.clear();
+
+	displayBookmarks();
+}//end function clearBookmarks
 
 //register event listeners
 function start()
 {
+
 	document.getElementById("saveButton").addEventListener(
 		"click",saveBookmark,false);
 
 	document.getElementById("clearButton").addEventListener(
 		"click", clearBookmarks,false);
+
+	displayBookmarks();
 }//end function start
 
 window.addEventListener("load", start, false);
